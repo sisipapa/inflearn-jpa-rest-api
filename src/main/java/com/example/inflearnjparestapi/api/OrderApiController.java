91178e7d888160796d8549a47a6c9e54139a9c54
sisipapa <sisiapap239@gmail.com>
2022-01-19 23:43:47 +0900

    M2

--- src/main/java/com/example/inflearnjparestapi/api/OrderApiController.java
+++ src/main/java/com/example/inflearnjparestapi/api/OrderApiController.java
@@ -32,7 +32,7 @@ public class OrderApiController {
     /**
      * Entity에 JsonIgnore 설정을 안해서 현재는 오류발생 강의 내용만 참고하자!!
      * M1
-     *
+     * M2
      * @return
      */
     @GetMapping("/api/v1/orders")
