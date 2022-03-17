a21f2882d04abc6a2b9004a1cd0e58a89e8fdd16
김경훈 <sisipapa@gmail.com>
2022-03-17 20:41:11 +0900

    QuerydslConfig 수정

--- src/main/java/com/example/inflearnjparestapi/config/QuerydslConfig.java
+++ src/main/java/com/example/inflearnjparestapi/config/QuerydslConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+
 @Configuration
 public class QuerydslConfig {
 
