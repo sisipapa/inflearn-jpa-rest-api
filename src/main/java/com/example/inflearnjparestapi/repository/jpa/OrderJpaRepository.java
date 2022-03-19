11fd7862dea6cbb000dcd9d8238bf1d02296c795
김경훈 <sisipapa@gmail.com>
2022-03-19 17:53:30 +0900

    Entitygraph 어노테이션 추가테스트

--- src/main/java/com/example/inflearnjparestapi/repository/jpa/OrderJpaRepository.java
+++ src/main/java/com/example/inflearnjparestapi/repository/jpa/OrderJpaRepository.java
@@ -1,12 +1,16 @@
 package com.example.inflearnjparestapi.repository.jpa;
 
 import com.example.inflearnjparestapi.domain.*;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface OrderJpaRepository extends JpaRepository<Order, Long> {
+
+    @EntityGraph(attributePaths = {"member", "delivery"}) // (2)
+    List<Order> findAll();
 
     Optional<List<Order>> findByMember(Member member);
 
